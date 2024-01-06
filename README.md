3ac5fbb1cc56aed0137c21e569805814df7eed22
Pham Le Thanh Nhan <phamlethanhnhan0707@gmail.com>
2024-01-07 01:54:24 +0700

    add code

---
+++ README.md
@@ -0,0 +1,19 @@
+# How to run our application
+
+1. Installation needed
+   - Node.js version 16
+   - Install MySQL version 8
+3. Running program
+
+    a. Database settings
+   - Create a database in MySQL
+   - Create tables in the database
+   - Insert data into the database
+   
+   b. Running program
+   - Open a terminal in VS Code (or cmd but prefer one in VS Code)
+   - Set directory to backend "cd backend" and install backend "npm install"
+   - Install the 'mysql2' library in npm "npm install mysql2"
+   - Open a new terminal, set the directory to frontend "cd frontend" and install frontend "npm install"
+   - Turn back terminal 1, start backend "npm start"
+   - In terminal 2, start frontend "npm start" and wait for opening the browser localhost:3000
---
+++ frontend/src/DocViewAppt.js
@@ -0,0 +1,103 @@
+import React, { Component} from 'react';
+import {
+    Box,
+    Button,
+    Heading,
+    Grommet,
+} from 'grommet';
+
+import './App.css';
+
+export class DocViewAppt extends Component {
+    state = { apptlist: [] }
+
+    componentDidMount() {
+        this.getNames("");
+    }
+    getNames(value) {
+        let docName = value;
+        console.log(docName);
+        fetch("http://localhost:3001/userInSession")
+            .then(res => res.json())
+            .then(res => {
+                var string_json = JSON.stringify(res);
+                var email_json = JSON.parse(string_json);
+                let email_in_use = email_json.email;
+                console.log(email_in_use);
+                fetch('http://localhost:3001/doctorViewAppt?email=' + email_in_use)
+                    .then(res => res.json())
+                    .then(res => {
+                        this.setState({ apptlist: res.data });
+                    });
+            });
+    }
+
+    render() {
+        const { apptlist } = this.state;
+        const Header = () => (
+            <Box
+                tag='header'
+                background='brand'
+                pad='small'
+                elevation='small'
+                justify='between'
+                direction='row'
+                align='center'
+                flex={false}
+            >
+                <a style={{ color: 'inherit', textDecoration: 'inherit'}} href="/"><Heading level='4' margin='none'>GROUP 07 - Hospital Data Management</Heading></a>
+            </Box>
+        );
+
+        const Body = () => (
+            <div className="container">
+                <div className="panel panel-default p50 uth-panel">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Appointment ID</th>
+                                <th>Patient</th>
+                                <th>Date</th>
+                                <th>Start Time</th>
+                                <th>Symptoms</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {apptlist.map(appt =>
+                                <tr key={appt.aid}>
+                                    <td>{appt.name}</td>
+                                    <td>{new Date(appt.date).toLocaleDateString().substring(0,10)} </td>
+                                    <td>{appt.starttime}</td>
+                                    <td>{appt.symptoms}</td>
+                                    <td>{appt.status}</td>
+                                    <td>
+                                        {appt.status === "NotDone"?
+                                            <Button label="Cancel"
+                                            onClick = {() => {
+                                                fetch('http://localhost:3001/deleteAppt?uid='+ appt.aid)
+                                                window.location.reload();
+                                            }}
+                                            ></Button>
+                                        :<div></div>}
+                                    </td> 
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        );
+        return (
+            <Grommet full={true}>
+                <Header />
+                <Box fill={true}>
+                    <Body />
+                </Box>
+            </Grommet>
+        );
+    }
+}
+
+export default DocViewAppt;
